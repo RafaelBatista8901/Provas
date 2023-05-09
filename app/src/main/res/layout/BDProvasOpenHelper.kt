a89b436f8ca0df93ea658cd53e28ca7d649f2e62
Rafael Batista <rafaelbatista8901@gmail.com>
2023-05-09 09:57:17 +0100

    Resolvido Erro criação da tabela de Provas

--- app/src/main/res/layout/BDProvasOpenHelper.kt
+++ app/src/main/res/layout/BDProvasOpenHelper.kt
@@ -4,19 +4,19 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import pt.ipg.provas.TabelaBD
-import pt.ipg.provas.TabelaCategorias
+import pt.ipg.provas.TabelaPercursos
 import pt.ipg.provas.TabelaProvas
 
 private const val VERSAO_BADE_DADOS = 1
 
 private const val NOME_BADE_DADOS = "Provas.db"
 
-class BDProvas(
+class BDProvasOpenHelper(
     context: Context?,
 ) : SQLiteOpenHelper(context, NOME_BADE_DADOS, null, VERSAO_BADE_DADOS) {
     override fun onCreate(db: SQLiteDatabase?) {
         requireNotNull(db)
-        TabelaCategorias(db).cria()
+        TabelaPercursos(db).cria()
         TabelaProvas(db).cria()
     }
 
