145ea3830e889e48f5b3457659ed4356f5d714b2
Rafael Batista <rafaelbatista8901@gmail.com>
2023-05-09 10:38:30 +0100

    Teste da criação da tabela Provas

--- app/src/main/java/pt/ipg/provas/BDProvasOpenHelper.kt
+++ app/src/main/java/pt/ipg/provas/BDProvasOpenHelper.kt
@@ -1,15 +1,10 @@
-package layout
+package pt.ipg.provas
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import pt.ipg.provas.TabelaBD
-import pt.ipg.provas.TabelaPercursos
-import pt.ipg.provas.TabelaProvas
 
 private const val VERSAO_BADE_DADOS = 1
-
-private const val NOME_BADE_DADOS = "Provas.db"
 
 class BDProvasOpenHelper(
     context: Context?,
@@ -22,5 +17,9 @@ class BDProvasOpenHelper(
 
     override fun onUpgrade(db: SQLiteDatabase?, OldVersion: Int, p2: Int) {
 
+    }
+
+    companion object {
+        const val NOME_BADE_DADOS = "Provas.db"
     }
 }
--- app/src/main/java/pt/ipg/provas/TabelaProvas.kt
+++ app/src/main/java/pt/ipg/provas/TabelaProvas.kt
@@ -5,7 +5,7 @@ import android.provider.BaseColumns
 
 class TabelaProvas(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, localidade TEXT NOT NULL, tipo TEXT NOT NULL, data DATE NOT NULL, id_Percursos INTEGER NOT NULL, FOREIGN KEY (id_Percursos) REFERENCES PERCURSOS ${TabelaPercursos.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, localidade TEXT NOT NULL, tipo TEXT NOT NULL, data DATE NOT NULL, id_Percursos INTEGER NOT NULL, FOREIGN KEY (id_Percursos) REFERENCES ${TabelaPercursos.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
