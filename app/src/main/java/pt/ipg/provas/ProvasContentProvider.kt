8b624866a32dc11b4a91ba7baa7c1852c32ad987
Rafael Batista <rafaelbatista8901@gmail.com>
2023-05-23 09:51:26 +0100

    Implemento o metodo query do content provider

--- app/src/main/java/pt/ipg/provas/ProvasContentProvider.kt
+++ app/src/main/java/pt/ipg/provas/ProvasContentProvider.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
 class ProvasContentProvider : ContentProvider() {
     private var bdOpenHelper : BDProvasOpenHelper?= null
@@ -15,13 +16,37 @@ class ProvasContentProvider : ContentProvider() {
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val bd = bdOpenHelper!!.readableDatabase
+        val id = uri.lastPathSegment
+
+        val endereco = uriMatcher().match(uri)
+
+        val tabela = when (endereco){
+            URI_PERCURSOS -> TabelaPercursos(bd)
+            URI_PERCURSO_ID -> TabelaProvas(bd)
+            URI_PROVAS -> TabelaProvas(bd)
+            URI_PROVA_ID -> TabelaProvas(bd)
+            else -> null
+        }
+
+        val (selecao, argsSel) = when (endereco) {
+            URI_PERCURSO_ID, URI_PROVA_ID -> Pair("${BaseColumns._ID}=?", arrayOf(id))
+            else -> Pair(selection, selectionArgs)
+        }
+
+        return tabela?.consulta(
+            projection as Array<String>,
+            selecao,
+            argsSel as Array<String>?,
+            null,
+            null,
+            sortOrder)
     }
 
     override fun getType(p0: Uri): String? {
@@ -46,10 +71,15 @@ class ProvasContentProvider : ContentProvider() {
         const val PERCURSO = "percurso"
 
         private const val URI_PERCURSOS = 100
+        private const val URI_PERCURSO_ID = 101
         private const val URI_PROVAS = 200
+        private const val URI_PROVA_ID = 201
+
         fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, PERCURSO, URI_PERCURSOS)
+            addURI(AUTORIDADE, "$PERCURSO", URI_PERCURSOS)
             addURI(AUTORIDADE, PROVAS, URI_PROVAS)
+            addURI(AUTORIDADE, "$PROVAS", URI_PROVAS)
         }
     }
 }
