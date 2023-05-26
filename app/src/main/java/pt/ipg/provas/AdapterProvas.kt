9876ccb537f33ade4c205c90e82beca01ffca133
Rafael Batista <rafaelbatista8901@gmail.com>
2023-05-26 09:18:44 +0100

    Criado adaptador de provas (faz o "interface" com o RecycleView)

---
+++ app/src/main/java/pt/ipg/provas/AdapterProvas.kt
@@ -0,0 +1,24 @@
+package pt.ipg.provas
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterProvas : RecyclerView.Adapter<AdapterProvas.ViewHolderProvas>() {
+    inner class ViewHolderProvas(itemView: View) : ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderProvas {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderProvas, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
