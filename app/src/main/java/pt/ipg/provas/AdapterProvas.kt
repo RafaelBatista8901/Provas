e1cfc3b85355cb251cd2ab66ab47eecdece3dd55
Rafael Batista <rafaelbatista8901@gmail.com>
2023-05-30 10:20:42 +0100

    Mostrar a prova no ViewHonder

--- app/src/main/java/pt/ipg/provas/AdapterProvas.kt
+++ app/src/main/java/pt/ipg/provas/AdapterProvas.kt
@@ -3,9 +3,9 @@ package pt.ipg.provas
 import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import java.util.PropertyResourceBundle
 
 class AdapterProvas(val fragment: ListaProvasFragment) : RecyclerView.Adapter<AdapterProvas.ViewHolderProvas>() {
     var cursor: Cursor? = null
@@ -15,7 +15,15 @@ class AdapterProvas(val fragment: ListaProvasFragment) : RecyclerView.Adapter<Ad
         }
 
     inner class ViewHolderProvas(contentor: View) : ViewHolder(contentor) {
-        internal var provas: Provas
+        private val textViewNome = contentor.findViewById<TextView>(R.id.textView_nome)
+        private val textViewPercurso = contentor.findViewById<TextView>(R.id.textView_Percurso)
+
+        internal var provas: Provas? = null
+            set(value){
+                field = value
+                textViewNome.text = provas?.nome ?: ""
+                textViewPercurso.text = provas?.id_Percursos.toString() ?: ""
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderProvas {
