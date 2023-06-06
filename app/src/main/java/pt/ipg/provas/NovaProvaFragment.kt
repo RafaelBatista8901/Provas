265623e22bce5ebaac89346ff1c46aeb8e1168de
Rafael Batista <rafaelbatista8901@gmail.com>
2023-06-06 11:25:14 +0100

    Adicionar fragment para adicionar a nova prova

---
+++ app/src/main/java/pt/ipg/provas/NovaProvaFragment.kt
@@ -0,0 +1,40 @@
+package pt.ipg.provas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import pt.ipg.provas.databinding.FragmentNovaPorvaBinding
+
+
+class NovaProvaFragment : Fragment() {
+    private var _binding: FragmentNovaPorvaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNovaPorvaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+            val activity = activity as MainActivity
+            activity.fragment = this
+            activity.IdMenuAtual = R.menu.menu_main
+        }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
