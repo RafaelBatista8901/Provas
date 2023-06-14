c741a210118f55733b6cc95b74a9f807d3e7d5fc
Rafael Batista <rafaelbatista8901@gmail.com>
2023-06-14 13:38:10 +0100

    Adicionado fragmento para eliminar prova e correspondente menu

---
+++ app/src/main/java/pt/ipg/provas/EliminarProvaFragment.kt
@@ -0,0 +1,82 @@
+package pt.ipg.provas
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import pt.ipg.provas.databinding.FragmentEliminarProvaBinding
+class EliminarProvaFragment : Fragment() {
+    private lateinit var Prova: Provas
+    private var _binding: FragmentEliminarProvaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentEliminarProvaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.IdMenuAtual = R.menu.menu_eliminar
+
+        Prova = EliminarProvaFragmentArgs.fromBundle(requireArguments()).prova
+
+        binding.textViewNome2Prova.text = Prova.nomeProva
+        binding.textViewLocalidade2Prova.text = Prova.localidade
+        binding.textViewTipo2Prova.text = Prova.tipo
+        binding.textViewData2Prova.text = Prova.data
+        binding.textViewPercurso2Prova.text = Prova.percursos.nomePercurso
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
+            }
+            R.id.action_cancelar -> {
+                voltaListaProvas()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaListaProvas() {
+        findNavController().navigate(R.id.action_eliminarProvaFragment_to_ListaProvasFragment)
+    }
+
+    private fun eliminar() {
+        val enderecoProva = Uri.withAppendedPath(ProvasContentProvider.ENDERECO_PROVAS, Prova.idProva.toString())
+        val numProvasEliminadas = requireActivity().contentResolver.delete(enderecoProva, null, null)
+
+        if (numProvasEliminadas == 1) {
+            Toast.makeText(requireContext(), getString(R.string.provas_eliminadas_com_sucesso), Toast.LENGTH_LONG).show()
+            voltaListaProvas()
+        } else {
+            Snackbar.make(binding.textViewNome2Prova, getString(R.string.erro_eliminar_livro), Snackbar.LENGTH_INDEFINITE)
+        }
+    }
+}
